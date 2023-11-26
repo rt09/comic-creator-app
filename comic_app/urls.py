6f99f7a975d35662407abcfef48a688f954d7682
rt09 <78974453+rt09@users.noreply.github.com>
2023-11-26 23:35:37 +0530

    ch

--- comic_app/urls.py
+++ comic_app/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('comic_generator.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
