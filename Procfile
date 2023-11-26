21903a8363f38025d42c915ae8032a748749ed0d
rt09 <78974453+rt09@users.noreply.github.com>
2023-11-26 23:20:15 +0530

    ch

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn mydjangoproject.wsgi --log-file -
--- build_files.sh
+++
@@ -1,4 +0,0 @@
-echo " BUILD START" 
-pip3 install -r requirements.txt 
-python3.9 manage.py collectstatic --noinput --clear 
-echo " BUILD END"
